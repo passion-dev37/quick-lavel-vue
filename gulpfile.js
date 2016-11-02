5fbd5d506959839915704b202dd668a94503b53f
passion-dev37 <passion.dev37@outlook.com>
2016-11-02 17:09:16 -0500

    walking-skeleton: Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-    .webpack('app.js');
+    mix
+        .sass('app.scss')
+        .webpack('app.js')
+        .browserSync({
+            proxy: 'vuejs-demo.dev'
+        });
 });
