8fe7176f27e8ccc7dc3770639312d99e08c89ef7
passion-dev37 <passion.dev37@outlook.com>
2016-11-03 05:23:45 -0500

    configure-vuejs-routing: Add initial admin and user vue components

---
+++ resources/assets/js/components/user-dashboard.vue
@@ -0,0 +1,31 @@
+<style scoped>
+</style>
+
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-4 col-md-push-4 col-sm-6 col-sm-push-3">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-push-2">
+                <h2>User</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {};
+        },
+        ready: function () {
+        },
+        methods: {
+            debug: function () {
+                console.debug('Loaded user dashboard')
+            }
+        },
+    };
+</script>
