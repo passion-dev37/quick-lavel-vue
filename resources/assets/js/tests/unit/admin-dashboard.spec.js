05288e5279582a436add38d5deabd4f3072b31f0
passion-dev37 <passion.dev37@outlook.com>
2016-11-03 14:18:47 -0500

    configure-vuejs-routing: Modify test

--- resources/assets/js/tests/unit/admin-dashboard.spec.js
+++ resources/assets/js/tests/unit/admin-dashboard.spec.js
@@ -1,5 +1,7 @@
 // Test File "admin-dashboard.spec.js"
 
+// Import Vue and the component being tested
+import Vue from 'vue'
 import AdminDashboard from '../../components/admin-dashboard.vue';
 
 describe('AdminDashboard', () => {
